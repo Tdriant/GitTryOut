0508153f12cffc4521f17e6c479a44f486334730
vallansell <vallan95@hotmail.fr>
2018-10-19 14:09:03 +0200

    voici mon fichier

---
+++ MavenMVT/src/main/java/mavenMVT/ClassC.java
@@ -0,0 +1,8 @@
+package mavenMVT;
+
+public class ClassC {
+	public void methode() {
+		System.out.println("Ceci est la seule méthode de classC");
+	}
+
+}
--- mavenMVT/src/classC.java
+++
@@ -1,8 +0,0 @@
-
-public class classC {
-	
-	void methode {
-		System.out.println("slt je suis la classe C");
-	}
-
-}
